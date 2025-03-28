fc75bdf78eba988260cd48ce83f93c2104a2eae4
mukuvi <jamesmngandu@gmail.com>
2025-03-28 14:04:12 +0300

    aquasense

--- frontend/mobile/screens/post.js
+++ frontend/mobile/screens/post.js
@@ -1,14 +1,24 @@
-import React, { useState, useEffect } from 'react';
-import { View, Text, TextInput, TouchableOpacity, StyleSheet, Image, Alert, Platform, ScrollView } from 'react-native';
-import { Ionicons, Entypo } from '@expo/vector-icons';
-import * as ImagePicker from 'expo-image-picker';
-import * as DocumentPicker from 'expo-document-picker';
-import axios from 'axios';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import { useNavigation, useRoute } from '@react-navigation/core';
-import { SERVER_IP } from './constants';
+import React, { useState, useEffect } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+  Image,
+  Alert,
+  Platform,
+  ScrollView,
+} from "react-native";
+import { Ionicons, Entypo } from "@expo/vector-icons";
+import * as ImagePicker from "expo-image-picker";
+import * as DocumentPicker from "expo-document-picker";
+import axios from "axios";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useNavigation, useRoute } from "@react-navigation/core";
+import { SERVER_IP } from "./constants";
 function Post() {
-  const [message, setMessage] = useState('');
+  const [message, setMessage] = useState("");
   const [images, setImages] = useState([]);
   const [files, setFiles] = useState([]);
   const [userId, setUserId] = useState(null);
@@ -44,7 +54,7 @@ function Post() {
   const handlePickFile = async () => {
     const result = await DocumentPicker.getDocumentAsync({ multiple: true });
 
-    if (result.type === 'success') {
+    if (result.type === "success") {
       if (Array.isArray(result.uri)) {
         setFiles([...files, ...result.uri]);
       } else {
@@ -56,13 +66,13 @@ function Post() {
   useEffect(() => {
     const fetchUserId = async () => {
       try {
-        const userData = await AsyncStorage.getItem('userData');
+        const userData = await AsyncStorage.getItem("userData");
         if (userData) {
           const { id } = JSON.parse(userData);
           setUserId(parseInt(id));
         }
       } catch (error) {
-        console.error('Erreur lors de la récupération de l\'ID utilisateur depuis AsyncStorage:', error);
+        console.error("Error retrieving user ID from AsyncStorage:", error);
       }
     };
 
@@ -75,7 +85,7 @@ function Post() {
         const response = await axios.get(`${SERVER_IP}/api/users/${userId}/`);
         setUser(response.data);
       } catch (error) {
-        console.error('Erreur lors de la récupération des données utilisateur:', error);
+        console.error("Error retrieving user data:", error);
       }
     };
 
@@ -87,58 +97,64 @@ function Post() {
   const uploadPost = async () => {
     try {
       const formData = new FormData();
-      formData.append('sender', parseInt(userId));
-      formData.append('contenu', message);
+      formData.append("sender", parseInt(userId));
+      formData.append("contenu", message);
 
       images.forEach((image, index) => {
         const type = getTypeFromExtension(image);
-        formData.append('images', { uri: image, name: `image_${index}.${type}`, type });
+        formData.append("images", {
+          uri: image,
+          name: `image_${index}.${type}`,
+          type,
+        });
       });
 
       files.forEach((file, index) => {
         const type = getTypeFromExtension(file);
-        const file_list = file.split('/');
+        const file_list = file.split("/");
         const name = file_list[file_list.length - 1];
-        formData.append('files', { uri: file, name, type });
+        formData.append("files", { uri: file, name, type });
       });
 
       const response = await axios.post(`${SERVER_IP}/api/partage/`, formData, {
         headers: {
-          'Content-Type': 'multipart/form-data'
-        }
+          "Content-Type": "multipart/form-data",
+        },
       });
 
       if (response.status === 201) {
-        Alert.alert('Success', 'La publication a été publiée');
-        navigation.navigate('Home');
+        Alert.alert("Success", "The post has been published");
+        navigation.navigate("Home");
       } else {
-        Alert.alert('Error', 'Failed to upload post');
+        Alert.alert("Error", "Failed to upload post");
       }
     } catch (error) {
-      console.error('Error uploading post:', error);
-      Alert.alert('Error', 'Failed to upload post');
+      console.error("Error uploading post:", error);
+      Alert.alert("Error", "Failed to upload post");
     }
   };
 
   const getTypeFromExtension = (fileName) => {
-    const extension = fileName.split('.').pop().toLowerCase();
+    const extension = fileName.split(".").pop().toLowerCase();
 
     switch (extension) {
-      case 'jpg':
-      case 'jpeg':
-        return 'image/jpeg';
-      case 'png':
-        return 'image/png';
-      case 'gif':
-        return 'image/gif';
-      case 'pdf':
-        return 'application/pdf';
-      case 'mp4':
-      case 'mov':
-      case 'avi':
-        return 'video/mp4';
+      case "jpg":
+      case "jpeg":
+        return "image/jpeg";
+      case "png":
+        return "image/png";
+      case "gif":
+        return "image/gif";
+      case "pdf":
+        return "application/pdf";
+      case "mp4":
+      case "mov":
+      case "avi":
+        return "video/mp4";
       default:
-        return Platform.OS === 'ios' ? 'application/octet-stream' : 'application/*';
+        return Platform.OS === "ios"
+          ? "application/octet-stream"
+          : "application/*";
     }
   };
 
@@ -146,19 +162,26 @@ function Post() {
     <ScrollView contentContainerStyle={styles.scrollContainer}>
       <View style={styles.container}>
         <View style={styles.container_header}>
-          <Text style={styles.placeholder}>Exprimez-vous</Text>
+          <Text style={styles.placeholder}>Express yourself</Text>
         </View>
         <View style={styles.personContainer}>
-          <Image source={user.profile_url ? { uri: user.profile_url } : require('./../assets/profil.jpg')} style={styles.personImage} />
-          <Text style={styles.personName}>{ user.nom}</Text>
+          <Image
+            source={
+              user.profile_url
+                ? { uri: user.profile_url }
+                : require("./../assets/profil.jpg")
+            }
+            style={styles.personImage}
+          />
+          <Text style={styles.personName}>{user.nom}</Text>
         </View>
         <TextInput
           style={styles.input}
-          placeholder="Exprimez-vous"
+          placeholder="Express yourself"
           value={message}
           onChangeText={(text) => setMessage(text)}
           multiline
-          textAlignVertical="top" 
+          textAlignVertical="top"
         />
         <View style={styles.iconContainer}>
           <TouchableOpacity style={styles.iconButton} onPress={handlePickImage}>
@@ -171,11 +194,11 @@ function Post() {
           </TouchableOpacity>
           <TouchableOpacity style={styles.iconButton} onPress={handlePickFile}>
             <Ionicons name="attach" size={24} color="#2ecc71" />
-            <Text style={styles.iconText}>Fichiers</Text>
+            <Text style={styles.iconText}>Files</Text>
           </TouchableOpacity>
         </View>
         <TouchableOpacity style={styles.shareButton} onPress={uploadPost}>
-          <Text style={styles.shareButtonText}>Partager</Text>
+          <Text style={styles.shareButtonText}>Share</Text>
         </TouchableOpacity>
       </View>
     </ScrollView>
@@ -185,27 +208,27 @@ function Post() {
 const styles = StyleSheet.create({
   scrollContainer: {
     flexGrow: 1,
-    justifyContent: 'center',
-    backgroundColor: 'white',
+    justifyContent: "center",
+    backgroundColor: "white",
   },
   container: {
     flex: 1,
     padding: 7,
-    backgroundColor: 'white',
+    backgroundColor: "white",
     top: 40,
   },
   container_header: {
     paddingBottom: 10,
     paddingHorizontal: 20,
-    alignItems: 'flex-start',
+    alignItems: "flex-start",
   },
   placeholder: {
     fontSize: 16,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   personContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
     marginBottom: 10,
   },
   personImage: {
@@ -216,40 +239,40 @@ const styles = StyleSheet.create({
   },
   personName: {
     fontSize: 16,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   input: {
     borderWidth: 1,
-    borderColor: '#ccc',
+    borderColor: "#ccc",
     borderRadius: 5,
     padding: 10,
     marginBottom: 10,
     height: 200,
   },
   iconContainer: {
-    flexDirection: 'row', 
-    justifyContent: 'space-around', 
+    flexDirection: "row",
+    justifyContent: "space-around",
     marginBottom: 20,
   },
   iconButton: {
-    alignItems: 'center',
+    alignItems: "center",
   },
   iconText: {
-    color: 'grey',
+    color: "grey",
     fontSize: 15,
-    fontWeight: 'bold',
-    marginLeft: 5, 
+    fontWeight: "bold",
+    marginLeft: 5,
   },
   shareButton: {
-    backgroundColor: '#2ecc71',
+    backgroundColor: "#2ecc71",
     paddingVertical: 13,
     borderRadius: 5,
-    alignItems: 'center',
+    alignItems: "center",
     marginHorizontal: 2,
   },
   shareButtonText: {
-    color: 'white',
-    fontWeight: 'bold',
+    color: "white",
+    fontWeight: "bold",
   },
 });
 
