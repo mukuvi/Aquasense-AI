63aa639211ff2ba4fdbfa02dc75e595337811bbf
mukuvi <jamesmngandu@gmail.com>
2025-03-28 14:24:48 +0300

    aquasense

--- frontend/mobile/screens/index.js
+++ frontend/mobile/screens/index.js
@@ -1,5 +1,5 @@
-import { AppRegistry } from 'react-native';
-import App from '../App'; // Assurez-vous que ce chemin est correct
-import { name as appName } from './app.json'; // Assurez-vous que ce fichier existe et contient le nom correct
+import { AppRegistry } from "react-native";
+import App from "../App"; // Make sure this path is correct
+import { name as appName } from "./app.json"; // Make sure this file exists and contains the correct name
 
 AppRegistry.registerComponent(appName, () => App);
