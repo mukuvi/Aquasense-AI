7144451b4b4882499a28dc46fadbbeb988701034
mukuvi <jamesmngandu@gmail.com>
2025-03-28 14:22:25 +0300

    aquasense

--- frontend/mobile/screens/Loader.js
+++ frontend/mobile/screens/Loader.js
@@ -1,49 +1,49 @@
-import React, { useEffect, useState } from 'react';
-import { Animated, Easing, StyleSheet, View } from 'react-native';
-import Welcome from './Welcome'; // Importez le composant Welcome
+import React, { useEffect, useState } from "react";
+import { Animated, Easing, StyleSheet, View } from "react-native";
+import Welcome from "./Welcome"; // Importez le composant Welcome
 
 const random = () => parseInt(Math.random() * 255);
-const randomColor = () => 'rgb(' + random() + ',' + random() + ',' + random() + ')';
+const randomColor = () =>
+  "rgb(" + random() + "," + random() + "," + random() + ")";
 
 const Dim = 50;
-
 export default function Loader5() {
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const timer = setTimeout(() => {
-      setIsLoading(false); // Après 3 secondes, passez à la page Welcome
+      setIsLoading(false); // After 3 seconds, transition to the Welcome page
     }, 3000);
 
-    return () => clearTimeout(timer);
+    return () => clearTimeout(timer); // Cleanup the timer when the component unmounts
   }, []);
 
   return isLoading ? (
     <View style={styles.container}>
-      {/* Votre Loader ici */}
+      {/* Add your loader, for example, an ActivityIndicator */}
     </View>
   ) : (
-    <Welcome /> // Passez à la page Welcome une fois le chargement terminé
+    <Welcome /> // Transition to the Welcome page once loading is complete
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
+    justifyContent: "center",
+    alignItems: "center",
   },
   item: {
     width: Dim,
     height: Dim,
     borderWidth: 0,
-    backgroundColor: 'transparent',
-    position: 'absolute',
-    justifyContent: 'center'
+    backgroundColor: "transparent",
+    position: "absolute",
+    justifyContent: "center",
   },
   innerItem: {
     height: 10,
     width: 10,
-    borderRadius: 10
-  }
+    borderRadius: 10,
+  },
 });
